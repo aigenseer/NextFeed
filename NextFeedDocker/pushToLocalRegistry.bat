229a8a5d600f63d053302cc5775ac4648c727805
aigenseer <viktor.aigenseer@web.de>
2022-10-09 12:55:47 +0200

    fix logging metrics

--- NextFeedDocker/pushToLocalRegistry.bat
+++ NextFeedDocker/pushToLocalRegistry.bat
@@ -2,7 +2,7 @@
 set version=%1
 set server=%2
 
-@REM Parameter: 0.0.1-SNAPSHOT 192.168.0.76:31091
+@REM Parameter: 0.0.1-SNAPSHOT 192.168.0.220:5005
 set list=user-repository-service system-repository-service survey-repository-service session-repository-service question-repository-service participant-repository-service mood-repository-service gateway-service authorization-external-service question-external-service session-external-service survey-external-service mood-manager-service participant-manager-service question-manager-service session-manager-service survey-manager-service survey-template-manager-service system-manager-service user-manager-service mood-socket-service question-socket-service session-socket-service survey-socket-service
 (for %%a in (%list%) do (
    echo %%a
