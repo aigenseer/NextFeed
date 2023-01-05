c4e5605ac3e7f00e2d5d49648ee3713e9f8b5328
aigenseer <viktor.aigenseer@web.de>
2023-01-05 22:51:58 +0100

    update docker version

--- NextFeedDocker/pushToLocalRegistry.bat
+++ NextFeedDocker/pushToLocalRegistry.bat
@@ -3,10 +3,10 @@ set version=%1
 set server=%2
 
 @REM Parameter: 0.0.1-SNAPSHOT 192.168.0.220:5005
-set list=session-service user-management-service survey-service question-service mood-service authorization-service
+set list=session-service survey-service question-service mood-service
 @REM gateway-service
 (for %%a in (%list%) do (
    echo %%a
-   docker tag nextfeed/kube-4-%%a:%version% %server%/kube-4-%%a
-   docker push %server%/kube-4-%%a
+   docker tag nextfeed/kube-5-%%a:%version% %server%/kube-5-%%a
+   docker push %server%/kube-5-%%a
 ))
